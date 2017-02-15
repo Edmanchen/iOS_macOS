919ea3b1f7660aae011a7cfd34d318162108a9f1
Edman Chen <edman@xmind.net>
2017-02-15 15:22:33 +0800

    awe

---
+++ Shared/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  CloudSearch
+//
+//  Created by Edman Chen on 2/15/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Shared/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  CloudSearch
+//
+//  Created by Edman Chen on 2/15/17.
+//
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
